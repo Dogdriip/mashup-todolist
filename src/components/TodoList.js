e0530dbfc054664390e0b2129c9d4ac7dab814d0
Hyunseung Jeon <dogdriip@gmail.com>
2020-11-02 13:54:24 +0900

    Add TodoItem

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,16 +1,22 @@
 import React from "react";
 import styled from "styled-components";
+import TodoItem from "./TodoItem";
 
 const TodoListBlock = styled.div`
   flex: 1;
   padding: 20px 32px 48px 32px;
   overflow-y: auto;
-
-  background-color: red;
 `;
 
 const TodoList = () => {
-  return <TodoListBlock>TodoListBlock</TodoListBlock>;
+  return (
+    <TodoListBlock>
+      <TodoItem text="테스트1" done={true} />
+      <TodoItem text="테스트2" done={true} />
+      <TodoItem text="테스트3" done={false} />
+      <TodoItem text="테스트4" done={false} />
+    </TodoListBlock>
+  );
 };
 
 export default TodoList;
